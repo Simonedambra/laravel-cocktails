f87da2cf1d3ca713fc504eccfdfd1a070f23c528
Simonedambra <simonedambra130102@gmail.com>
2024-07-11 16:47:03 +0200

    fix

--- database/migrations/2024_07_11_140840_create_cocktails_table.php
+++ database/migrations/2024_07_11_140840_create_cocktails_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('cocktails', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 50);
+            $table->string('ingredienti', 200);
+            $table->string('base_alcolica', 150);
+            $table->string('paese_di_origine', 50);
+            $table->boolean('is_alcoholic')->default(true);
+            $table->string('flavor', 50);
             $table->timestamps();
         });
     }
